3612c820496d499a213cae9e89c6170c31e65fa1
Ahmed Mohamed 3adl <80198727+Ahmed101Mohammed@users.noreply.github.com>
2022-08-20 12:10:11 +0200

    Building the structure of the server.
    Modify the main.js file.
    - Add modules.
    - Add middleware.
    - Add the server and run it.

---
+++ main.js
@@ -0,0 +1,28 @@
+// required medules:
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const env = require('dotenv').config();
+const path = require('path');
+const cookieParser = require('cookie-parser');
+// server:
+const app = express();
+const server = require('http').createServer(app);
+
+// middlewares:
+app.use(cors);
+app.use(express.json());
+app.use(express.static(path.join('public')));
+app.use(express.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cookieParser());
+app.disable('x-powered-by');
+
+// routers:
+
+// run server:
+const port = process.env.PORT || 8000;
+server.listen(port,()=>{
+    console.log(`Your server is runing... \nin port: ${port}`);
+})
