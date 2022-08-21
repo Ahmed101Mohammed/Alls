d17a384bbdb567556583b85fce1b5cd75a604861
Ahmed Mohamed 3adl <80198727+Ahmed101Mohammed@users.noreply.github.com>
2022-08-21 13:21:46 +0200

    Creating two arrows to move between products.
    Creating two arrows to move between products in any category.
    I create a new file :
    1 - controllingCAnglesApearance.js: this file has three functions:
    - appearAngles.
    - disappearAngles.
    - giveEvents.
    
    I modify domElements.js file: In this file, I add a new function:
    - getCategories.

---
+++ public/javaScriptFiles/index/controllingCAnglesApearavce.js
@@ -0,0 +1,32 @@
+// In this file: will take all categories and give them an event to appear angles when touch them by user mouse pinter.
+
+// appearAngles: this function appear Angles when the mouse hover in the  category section.
+const appearAngles = (e)=>{
+    let rAngle = e.currentTarget.querySelector('.right-angle');
+    let lAngle = e.currentTarget.querySelector('.left-angle');
+    rAngle.style.display = 'flex';
+    lAngle.style.display = 'flex';
+}
+
+// disappearAngles: this function disappear Angles when the mouse go out from the  category section.
+const disappearAngles = (e)=>{
+    let rAngle = e.currentTarget.querySelector('.right-angle');
+    let lAngle = e.currentTarget.querySelector('.left-angle');
+    rAngle.style.display = 'none';
+    lAngle.style.display = 'none';
+    console.log('why')
+}
+
+// giveEvents : this function giv all categories in home page this events mouseover ==> appearAngles, mouseout ==> disappearAngles.
+const giveEvents = ()=>{
+    let categories = getCategories();
+
+    for(let i of categories)
+    {
+        i.addEventListener('mouseover',appearAngles);
+        i.addEventListener('mouseout',disappearAngles);
+    }
+}
+
+
+giveEvents();// function run. 
--- public/javaScriptFiles/index/domElements.js
+++ public/javaScriptFiles/index/domElements.js
@@ -1,4 +1,5 @@
 // In this file I determine all elements that I will need in other files.
 
 const productsContainers = ()=> document.querySelectorAll('div.category div.container');// determine all products containers in the home page.
-const containerProducts = ()=> document.querySelectorAll('div.category div.container div.product');
+const containerProducts = ()=> document.querySelectorAll('div.category div.container div.product');// determin all products.
+const getCategories = ()=> document.querySelectorAll('main div.category');// determin all categories. 
