96a1a7cdd5a497893e3b44e96fecd11a568b91f8
Ahmed Mohamed 3adl <80198727+Ahmed101Mohammed@users.noreply.github.com>
2022-09-17 18:53:52 +0200

    Delete hasUnWontedChar.js

--- algos/hasUnWontedChar.js
+++
@@ -1,7 +0,0 @@
-// The algorithm in this file will fined if tha password have un wanted letters or not.
-
-// modules:
-// const path = require('path');
-// const hashMapForSpecialChars = require(path.join(__dirname,'..','classes','HashMapForSpecialChars.js'));
-
-// hasUnWantedSp
