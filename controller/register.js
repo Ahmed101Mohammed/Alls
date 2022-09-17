83625f249600a9a8552eabe3f7a8fed78e9d058a
Ahmed Mohamed 3adl <80198727+Ahmed101Mohammed@users.noreply.github.com>
2022-09-17 18:48:40 +0200

    Complate registerPostUserData function

--- controller/register.js
+++ controller/register.js
@@ -10,8 +10,38 @@ const registerPostUserData = async(req,res)=>{
     const {userName,email,password} = req.body;
     console.log({userName,email,password})
     
-    Register.isACorrectInputs({userName,email,password},res);
-    
+    let isACorrectInputs = Register.isACorrectInputs({userName,email,password},res);
+    console.log('all inputs is good')
+    if(Register.notEqualTrueString(isACorrectInputs)) return;
+    console.log('all inputs is good')
+    await User.findOne({userName:userName}).then(user=>{
+        console.log('u',user);
+        if(user !== null && user.userName === userName)
+        {
+            return res.status(301).json({error: 'Sorry the userName was used before'})
+        }
+    });
+
+    try{
+        let hashedPpassword = await bcrypt.hash(password,10);
+
+        let user = new User({
+            _id: 1,
+            userName: userName,
+            email: email,
+            password: hashedPpassword
+        });
+
+        let newUser = await user.save()
+
+        console.log(newUser);
+
+        return res.status(200).json({'successRegister': 'Now you have an account on ALLs website'})
+    }
+    catch(e)
+    {
+        return res.status(500).json({'fieldRegister': 'Server error the account field to creating, Try again'})
+    }
 
 }
 
