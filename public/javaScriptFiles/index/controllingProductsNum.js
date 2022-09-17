329a6cab1fabdc26dcf3447a2a00aa4d78494520
Ahmed Mohamed 3adl <80198727+Ahmed101Mohammed@users.noreply.github.com>
2022-09-17 15:00:57 +0200

    Create Containers object and add some methods.
    Methods added:
    - getAllContainers
    - getNumberFrom0To20
    - getIndexsOfTargetProducts
    - generate20StaticProducts
    - createContainerObject
    - addTargetProductsToTheContainer
    - generateProductsInAllContainers
    - changeProductsWidth
    - generateProductsAndFixItsWidth

--- public/javaScriptFiles/index/controllingProductsNum.js
+++ public/javaScriptFiles/index/controllingProductsNum.js
@@ -1,7 +1,127 @@
 // In this file the scripting determines the number of products in the Categories parts.
+const Containers = {
+    getAllContainers:()=> document.querySelectorAll('div.category div.container'),
+    categoriesObjects: [],
+    externalMethods:{
+        generateProduct: (num)=>{
+            return `
+                        <!-- product card -->
+                        <div class="product">
 
+                            <!-- product photo -->
+                            <div class="photo">
+                                <img src="https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg" alt="canone camera">
+                            </div>
+
+                            <!-- product details -->
+                            <div class="details">
+                                <!-- product title -->
+                                <h3 class="title">product title${num}</h3>
+
+                                <!-- product rate-->
+                                <div class="rate">
+                                    <i class="fa-solid fa-star s1"></i>
+                                    <i class="fa-solid fa-star s2"></i>
+                                    <i class="fa-solid fa-star s3"></i>
+                                    <i class="fa-solid fa-star s4"></i>
+                                    <i class="fa-solid fa-star s5"></i>
+                                </div>
+
+                                
+                                <span class="avilable">avilable</span>
+                                <span class="price">50$</span>
+                            </div>
+
+                        </div>
+                    `
+        }
+    },
+
+    getNumberFrom0To20: (theNumber)=>{
+        let number = theNumber % 20;
+        
+        if(number < 0)
+        {
+            number += 20;
+        }
+
+        return number;
+    },
+
+    getIndexsOfTargetProducts: (TheStartedIndex,numOfWantedProducts)=>{
+
+        TheStartedIndex = Containers.getNumberFrom0To20(TheStartedIndex - 1);
+
+        let indexsOfWantedProducts = [];
+
+        for(let productNumber = 0; productNumber < numOfWantedProducts; productNumber++)
+        {
+            TheStartedIndex = Containers.getNumberFrom0To20(TheStartedIndex);
+    
+            indexsOfWantedProducts.push(TheStartedIndex);
+    
+            TheStartedIndex--;
+        }
+
+        return indexsOfWantedProducts;
+    },
+
+    generate20StaticProducts: ()=>{
+        let products = [];
+    
+        for(let productNumber = 0; productNumber < 20; productNumber++)
+        {
+            products.push(Containers.externalMethods.generateProduct(productNumber + 1));
+        }
+    
+        return products;
+    },
+
+    createContainerObject: (containerElements,TheStartedIndex,numberOfWantedProducts)=>{
+        let id = containerElements.parentElement.id;
+        Containers.categoriesObjects[Number(id)] = new Container();
+        Containers.categoriesObjects[Number(id)].index = TheStartedIndex;
+        Containers.categoriesObjects[Number(id)].num = numberOfWantedProducts;
+    },
+
+    addTargetProductsToTheContainer: (indexsOfTargetProducts,productsList,theContainer)=>{
+
+        for(let index = indexsOfTargetProducts.length-1 ; index > -1; index--)
+            {
+                theContainer.innerHTML += productsList[indexsOfTargetProducts[index]];
+            }
+    },
+
+    generateProductsInAllContainers: (TheStartedIndex,numberOfWantedProducts)=>{
+
+    
+        let products = Containers.generate20StaticProducts();
+        let indexs = Containers.getIndexsOfTargetProducts(TheStartedIndex,numberOfWantedProducts);
+        let allContainers = Containers.getAllContainers();
+        
+        for(let container of allContainers)
+        {
+            Containers.createContainerObject(container,TheStartedIndex,numberOfWantedProducts);
+            container.innerHTML = '';
+            Containers.addTargetProductsToTheContainer(indexs,products,container);
+        }
+            
+    },
+
+    changeProductsWidth: (numOfProducts, products)=>{
+        for(let product of products)
+        {
+            product.style.width = `${100 / numOfProducts}%`;
+        }
+    },
+
+    generateProductsAndFixItsWidth: (theStartedIndex,productsNumber)=>{
+        Containers.generateProductsInAllContainers(theStartedIndex,productsNumber);
+        Containers.changeProductsWidth(productsNumber,containerProducts());
+    }
+};
 // class category: had the index and num atributes.
-class category{
+class Container{
     constructor()
     {
         this.index = 0;
@@ -21,87 +141,6 @@ class category{
 
 let categoriesObjects = []; // this variabel save all category objects created.
 
-// indexsTarget: this function give me the indexs of the pruducts thats will be view.
-const indexsTarget = (index,num)=>{
-    let start = (index % 20) -1;
-
-    if (start < 0)
-    {
-        start = 20 + start;
-    }
-
-    let indexs = [];
-    for(let i = 0; i < num; i++)
-    {
-        start = (start >= 0)? start:20 + start;
-        indexs.push(start);
-        start--;
-    }
-    return indexs;
-}
-
-// repeateProducts: this function take num and element: it and the copies of this elemet to the original element and return it.
-const repeateProducts = ()=>{
-    let elements = [];
-
-    for(let i = 0; i < 20; i++)
-    {
-        elements.push(`
-                            <!-- product card -->
-                            <div class="product">
-
-                                <!-- product photo -->
-                                <div class="photo">
-                                    <img src="https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg" alt="canone camera">
-                                </div>
-
-                                <!-- product details -->
-                                <div class="details">
-                                    <!-- product title -->
-                                    <h3 class="title">product title${i+1}</h3>
-
-                                    <!-- product rate-->
-                                    <div class="rate">
-                                        <i class="fa-solid fa-star s1"></i>
-                                        <i class="fa-solid fa-star s2"></i>
-                                        <i class="fa-solid fa-star s3"></i>
-                                        <i class="fa-solid fa-star s4"></i>
-                                        <i class="fa-solid fa-star s5"></i>
-                                    </div>
-
-                                    
-                                    <span class="avilable">avilable</span>
-                                    <span class="price">50$</span>
-                                </div>
-
-                            </div>
-                        `);
-    }
-
-    return elements;
-}
-
-// generateProducts: this function take containers and number: it generate products the containers wiht the numper of repeatetion = num in all container. 
-const generateProducts = (index,num,containers)=>{
-
-    
-    let products = repeateProducts();
-    let indexs = indexsTarget(index,num);
-    //console.log({products,indexs,containers});
-    for(let i of containers)
-    {
-        let id = i.parentElement.id;
-        categoriesObjects[Number(id)] = new category();
-        categoriesObjects[Number(id)].index = index;
-        categoriesObjects[Number(id)].num = num;
-        i.innerHTML = '';
-        for(let j = indexs.length-1 ; j > -1; j--)
-        {
-            i.innerHTML += products[indexs[j]];
-        }
-    }
-        
-}
 
 // chose class name
 const choseClass = (num)=>{
@@ -142,9 +181,10 @@ const choseClassL = (num)=>{
             return 'lp7';
     }
 }
+
 // generateProducts: this function take containers and number: it generate products the containers wiht the numper of repeatetion = num in all container. 
 const moveRight = (containerObject,container)=>{
-    let products = repeateProducts();
+    let products = Containers.generate20StaticProducts();
     containerObject.plusIndex();
     let index = (containerObject.index % 20) - 1;
     index = (index < 0)? 20 + index: index;
@@ -166,7 +206,7 @@ const moveRight = (containerObject,container)=>{
 
 // generateProducts: this function take containers and number: it generate products the containers wiht the numper of repeatetion = num in all container. 
 const moveLeft = (containerObject,container)=>{
-    let products = repeateProducts();
+    let products = Containers.generate20StaticProducts();
     containerObject.muinsIndex();
 
     let num = containerObject.num;
@@ -188,59 +228,51 @@ const moveLeft = (containerObject,container)=>{
     },600)
        
 }
-// changeProductsWidth: this function take num and all products and resize the width of products to equal : 100%/n;
-const changeProductsWidth = (num, products)=>{
-    for(let i of products)
-    {
-        i.style.width = `${100 / num}%`;
-    }
-}
-const reViewProduct = ()=>{
+
+const generateProductsAccordingWindowWidth = ()=>{
     if(window.innerWidth > 1650)
     {
-        const productsNum = 7;
-        generateProducts(productsNum,7,productsContainers());
-        changeProductsWidth(7,containerProducts());
+        Containers.generateProductsAndFixItsWidth(7,7);
     }
     else if(window.innerWidth > 1530)
     {
         const productsNum = 6;
-        generateProducts(productsNum,6,productsContainers());
-        changeProductsWidth(6,containerProducts());
+        Containers.generateProductsInAllContainers(6,productsNum);
+        Containers.changeProductsWidth(6,containerProducts());
     }
     else if(window.innerWidth > 1200)
     {
         const productsNum = 5;
-        generateProducts(productsNum,5,productsContainers());
-        changeProductsWidth(5,containerProducts());
+        Containers.generateProductsInAllContainers(5,productsNum);
+        Containers.changeProductsWidth(5,containerProducts());
     }
     else if(window.innerWidth > 1000)
     {
         const productsNum = 4;
-        generateProducts(productsNum,4,productsContainers());
-        changeProductsWidth(4,containerProducts());
+        Containers.generateProductsInAllContainers(4,productsNum);
+        Containers.changeProductsWidth(4,containerProducts());
     }
     else if(window.innerWidth > 730)
     {
         const productsNum = 3;
-        generateProducts(productsNum,3,productsContainers());
-        changeProductsWidth(3,containerProducts());
+        Containers.generateProductsInAllContainers(3,productsNum);
+        Containers.changeProductsWidth(3,containerProducts());
     }
     else if(window.innerWidth > 455)
     {
         const productsNum = 2;
-        generateProducts(productsNum,2,productsContainers());
-        changeProductsWidth(2,containerProducts());
+        Containers.generateProductsInAllContainers(2,productsNum);
+        Containers.changeProductsWidth(2,containerProducts());
     }
     else if(window.innerWidth < 455)
     {
         const productsNum = 1;
-        generateProducts(productsNum,1,productsContainers());
-        changeProductsWidth(1,containerProducts());
+        Containers.generateProductsInAllContainers(1,productsNum);
+        Containers.changeProductsWidth(1,containerProducts());
     }
 
 }
 
-window.addEventListener('resize',reViewProduct); // set the number of products when the page window resize;
+window.addEventListener('resize',generateProductsAccordingWindowWidth); // set the number of products when the page window resize;
 
-reViewProduct(); // set the number of products when page refresh;
+generateProductsAccordingWindowWidth(); // set the number of products when page refresh;
--- public/javaScriptFiles/index/domElements.js
+++ public/javaScriptFiles/index/domElements.js
@@ -1,5 +1,3 @@
 // In this file I determine all elements that I will need in other files.
-
-const productsContainers = ()=> document.querySelectorAll('div.category div.container');// determine all products containers in the home page.
 const containerProducts = ()=> document.querySelectorAll('div.category div.container div.product');// determin all products.
 const getCategories = ()=> document.querySelectorAll('main div.category');// determin all categories. 
