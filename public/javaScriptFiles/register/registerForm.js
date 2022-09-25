fa9bb75b5fed20238a78eee6f14208a2bba0d845
Ahmed Mohamed 3adl <80198727+Ahmed101Mohammed@users.noreply.github.com>
2022-09-25 16:01:42 +0200

    Add new Object to RegisterForm Object.
    The new Object called: postUserDataInRegisterRoute

--- public/javaScriptFiles/register/registerForm.js
+++ public/javaScriptFiles/register/registerForm.js
@@ -31,31 +31,47 @@ let RegisterForm = {
         }
     },
 
-    postUserDataInRegisterRoute: async(userRegisterData)=>{
-        const response = await fetch('/register',{
-            method:'POST',
-            credentials:'same-origin',
-            headers:{'Content-Type': 'application/json',},
-            body: JSON.stringify(userRegisterData),
-        })
+    postUserDataInRegisterRoute: {
+        mainMethod: async(userRegisterData)=>{
+            const response = await fetch('/register',{
+                method:'POST',
+                credentials:'same-origin',
+                headers:{'Content-Type': 'application/json',},
+                body: JSON.stringify(userRegisterData),
+            })
 
-        try{
-            const responseMessage = new response.json();
-            console.log(responseMessage);
-            return responseMessage;
-        }
-        catch(e)
-        {
-           console.error({"FielsdToSend":"No Response"})
+            let theResponseInJsonStructure = await RegisterForm.postUserDataInRegisterRoute.turnTheRespondToJsonHandlingError(response);
+            return theResponseInJsonStructure;
+        },
+
+        turnTheRespondToJsonHandlingError: async(theResponse)=>{
+            let theResponseInJson;
+            try{
+                const responseMessage = await theResponse.json();
+                theResponseInJson = responseMessage;
+            }
+            catch(e)
+            {
+                theResponseInJson = {"readingResponseError":"Field to read the response"};
+            }
+            return theResponseInJson;
         }
     },
 
-    sendDataToTheServer: ()=>{
-        const userName = RegisterForm.fieldsetElements.userName.inputElement();
-        const email = RegisterForm.fieldsetElements.email.inputElement();
-        const password = RegisterForm.fieldsetElements.password.inputElement();
+    sendDataToTheServer: async()=>{
+        const userNameValue = RegisterForm.fieldsetElements.userName.inputElement().value;
+        const emailValue = RegisterForm.fieldsetElements.email.inputElement().value;
+        const passwordValue = RegisterForm.fieldsetElements.password.inputElement().value;
+        
+        const data = {
+            userName: userNameValue,
+            email: emailValue,
+            password: passwordValue
+        };
 
-        const data = {userName,email,password};
+        let response = await RegisterForm.postUserDataInRegisterRoute.mainMethod(data);
+        return response;
     }  
 }
 
+RegisterForm.fieldsetElements.register.registerButton().addEventListener('click',RegisterForm.sendDataToTheServer)
