ddfb3b6b159b993845821a768ccea086c23ba0a1
Ahmed Mohamed 3adl <80198727+Ahmed101Mohammed@users.noreply.github.com>
2022-10-08 13:16:23 +0200

    Add three getter algos to 'registerPage' class
    The three algorithms are named:
    - userNameValue.
    - emailValue.
    - passwordValue.

--- public/javaScriptFiles/register/registerPageClass.js
+++ public/javaScriptFiles/register/registerPageClass.js
@@ -6,6 +6,22 @@ class RegisterPage
     }
 
 
+
+    get userNameValue()
+    {
+        return document.querySelector('#user-name').value;
+    }
+
+    get emailValue()
+    {
+        return document.querySelector('#email').value;
+    }
+
+    get passwordValue()
+    {
+        return document.querySelector('#pssw').value;
+    }
+
 }
 
 let myRegisterPage = new RegisterPage();
