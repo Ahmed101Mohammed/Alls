a9abfcf63308279ad94bf104efcbf3acd490f3a5
Ahmed Mohamed 3adl <80198727+Ahmed101Mohammed@users.noreply.github.com>
2022-10-11 09:31:31 +0200

    Replace the RegisterPage class
    Replace the RegisterPage class whit the RegisterPage factory

--- public/javaScriptFiles/register/registerPageClass.js
+++ public/javaScriptFiles/register/registerPageClass.js
@@ -1,141 +1,147 @@
-class RegisterPage
+function RegisterPage()
 {
-    constructor()
-    {
-        this.registerButton = document.querySelector('fieldset.register div.register');
-    }
+    let registerPageObject = {
+        registerButton: document.querySelector('fieldset.register div.register'),
 
 
-
-    get userNameValue()
-    {
-        return document.querySelector('#user-name').value;
-    }
-
-    get emailValue()
-    {
-        return document.querySelector('#email').value;
-    }
-
-    get passwordValue()
-    {
-        return document.querySelector('#pssw').value;
-    }
-
-
-    async handlingErrorOfTurnTheRespondToJsonFunctionality(theResponse)
-    {
-        let theResponseInJson;
-        try{
-            const responseMessage = await theResponse.json();
-            theResponseInJson = responseMessage;
-        }
-        catch(e)
+        getUserNameValue()
         {
-            theResponseInJson = {"readingResponseError":"Field to read the response"};
-        }
-        return theResponseInJson;
-    }
+            return document.querySelector('#user-name').value;
+        },
 
-    async postUserDataInRegisterRoute(userRegisterData)
-    {
-        const response = await fetch('/register',{
-            method:'POST',
-            credentials:'same-origin',
-            headers:{'Content-Type': 'application/json',},
-            body: JSON.stringify(userRegisterData),
-        })
-
-        let theResponseInJsonStructure = await this.handlingErrorOfTurnTheRespondToJsonFunctionality(response);
-        return theResponseInJsonStructure;
-    }
-
-
-    addTheErrorElementToTheFieldsetElement(errorElement, fieldsetElement)
-    {
-        fieldsetElement.appendChild(errorElement);
-        let errorElementContent = errorElement.textContent;
-        errorElement.textContent = "";
-
-        setTimeout(()=>{
-            errorElement.classList.add('errorMessageSecondApearance');
-        },0)
-        setTimeout(()=>{
-            errorElement.textContent = errorElementContent;
-        },600)
-    }
-
-    getFieldsetThatCausedTheError(errorMessage)
-    {
-        let theFieldsetThatCausedTheError;
-
-        if("userNameError" in errorMessage)
+        getEmailValue()
         {
-            theFieldsetThatCausedTheError = documet.querySelector('fieldset.user-name');
-            
-        }
-        else if("passwordError" in errorMessage)
+            return document.querySelector('#email').value;
+        },
+
+        getPasswordValue()
         {
-            theFieldsetThatCausedTheError = documet.querySelector('fieldset.email');
-        }
-        else if("emailError" in errorMessage)
+            return document.querySelector('#pssw').value;
+        },
+
+
+        async handlingErrorOfTurnTheRespondToJsonFunctionality(theResponse)
         {
-            theFieldsetThatCausedTheError = documet.querySelector('fieldset.pssw');
-        }
+            let theResponseInJson;
+            try{
+                const responseMessage = await theResponse.json();
+                theResponseInJson = responseMessage;
+            }
+            catch(e)
+            {
+                theResponseInJson = {"readingResponseError":"Field to read the response"};
+            }
+            return theResponseInJson;
+        },
 
-        return theFieldsetThatCausedTheError;
-    }
+        async postUserDataInRegisterRoute(userRegisterData)
+        {
+            const response = await fetch('/register',{
+                method:'POST',
+                credentials:'same-origin',
+                headers:{'Content-Type': 'application/json',},
+                body: JSON.stringify(userRegisterData),
+            })
 
-    cteateErrorElement(errorMessage)
-    {
-        let errorElement = document.createElement("div");
-        errorElement.classList.add("errorMessageFirstApearance");
-        let errorMessageProperity = Object.getOwnPropertyNames(errorMessage);
-        let errorMessageString = errorMessage[errorMessageProperity[0]];
-        errorElement.textContent = errorMessageString;
+            let theResponseInJsonStructure = await myRegisterPage.handlingErrorOfTurnTheRespondToJsonFunctionality(response);
+            return theResponseInJsonStructure;
+        },
 
-        return errorElement;
-    }
 
-    get lastErrorMessageElement()
-    {
-        return document.querySelector("form .errorMessageSecondApearance");
-    }
+        addTheErrorElementToTheFieldsetElement(errorElement, fieldsetElement)
+        {
+            fieldsetElement.appendChild(errorElement);
+            let errorElementContent = errorElement.textContent;
+            errorElement.textContent = "";
 
-    #appearErrorMessageAccordingErrorType()
-    {
-        let lastErrorElement = this.lastErrorMessageElement();
-        if(lastErrorElement) lastErrorElement.remove();
+            setTimeout(()=>{
+                errorElement.classList.add('errorMessageSecondApearance');
+            },0)
+            setTimeout(()=>{
+                errorElement.textContent = errorElementContent;
+            },600)
+        },
 
-        let errorElement = this.cteateErrorElement(errorMessage);
+        getFieldsetThatCausedTheError(errorMessage)
+        {
+            let theFieldsetThatCausedTheError;
+
+            if("userNameError" in errorMessage)
+            {
+                theFieldsetThatCausedTheError = document.querySelector('fieldset.user-name');
                 
-        let theParentElementOfErrorElement = this.getFieldsetThatCausedTheError(errorMessage);
-        if(!theParentElementOfErrorElement) return;
+            }
+            else if("passwordError" in errorMessage)
+            {
+                theFieldsetThatCausedTheError = document.querySelector('fieldset.pssw');
+            }
+            else if("emailError" in errorMessage)
+            {
+                
+                theFieldsetThatCausedTheError = document.querySelector('fieldset.email');
+            }
 
-        this.addTheErrorElementToTheFieldsetElement(errorElement,theParentElementOfErrorElement);
+            return theFieldsetThatCausedTheError;
+        },
+
+        cteateErrorElement(errorMessage)
+        {
+            let errorElement = document.createElement("div");
+            errorElement.classList.add("errorMessageFirstApearance");
+            let errorMessageProperity = Object.getOwnPropertyNames(errorMessage);
+            let errorMessageString = errorMessage[errorMessageProperity[0]];
+            errorElement.textContent = errorMessageString;
+
+            return errorElement;
+        },
+
+        getLastErrorMessageElement()
+        {
+            return document.querySelector("form .errorMessageSecondApearance");
+        },
+
+        appearErrorMessageAccordingErrorType(errorMessage)
+        {
+            let lastErrorElement = myRegisterPage.getLastErrorMessageElement();
+            if(lastErrorElement) lastErrorElement.remove();
+
+            let errorElement = myRegisterPage.cteateErrorElement(errorMessage);
+                    
+            let theParentElementOfErrorElement = myRegisterPage.getFieldsetThatCausedTheError(errorMessage);
+            if(!theParentElementOfErrorElement) return;
+
+            myRegisterPage.addTheErrorElementToTheFieldsetElement(errorElement,theParentElementOfErrorElement);
+        },
+
+
+        prapereTheUserRegisterData()
+        {
+            const userNameValue = myRegisterPage.getUserNameValue();
+            const emailValue = myRegisterPage.getEmailValue();
+            const passwordValue = myRegisterPage.getPasswordValue();
+            
+            const userRegisterData = {
+                userName: userNameValue,
+                email: emailValue,
+                password: passwordValue
+            };
+
+            return userRegisterData;
+        },
+
+
+        async sendUserRegisterDataToTheServer()
+        {
+            let userRegisterData = myRegisterPage.prapereTheUserRegisterData();
+            console.log({userRegisterData})
+            let response = await myRegisterPage.postUserDataInRegisterRoute(userRegisterData);
+            myRegisterPage.appearErrorMessageAccordingErrorType(response);
+        },
     }
-
-    prapereTheUserRegisterData()
-    {
-        const userNameValue = this.userNameValue();
-        const emailValue = this.emailValue();
-        const passwordValue = this.passwordValue();
-        
-        const userRegisterData = {
-            userName: userNameValue,
-            email: emailValue,
-            password: passwordValue
-        };
-
-        return userRegisterData;
-    }
-
-    async sendUserRegisterDataToTheServer()
-    {
-        let userRegisterData = this.prapereTheUserRegisterData();
-        let response = await this.postUserDataInRegisterRoute(userRegisterData);
-        this.#appearErrorMessageAccordingErrorType(response);
-    }
+    return registerPageObject;
 }
 
-let myRegisterPage = new RegisterPage();
+let myRegisterPage = RegisterPage();
+console.log(myRegisterPage)
+
+myRegisterPage.registerButton.addEventListener('click',myRegisterPage.sendUserRegisterDataToTheServer);
