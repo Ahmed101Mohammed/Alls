b7339519ff897da0968baab6fcd969d5425e78c2
Ahmed Mohamed 3adl <80198727+Ahmed101Mohammed@users.noreply.github.com>
2022-11-09 08:25:53 +0200

    Add a post request to send sign data for Auth

--- routers/sign.js
+++ routers/sign.js
@@ -1,11 +1,13 @@
 // modules
 const router = require('express').Router();
 const path = require('path');
+const signToMyAccount = require(path.join(__dirname, '..', 'controller', 'sign.js'));
 
 // routes
 router.route('/')
     .get((req,res)=>{
         res.sendFile(path.join(__dirname,'..','views','sign.html'))
     })
+    .post(signToMyAccount);
 
 module.exports = router;
