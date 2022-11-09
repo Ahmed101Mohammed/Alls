fbab846e40c5e6b08bf21871e77a00a74958ba49
Ahmed Mohamed 3adl <80198727+Ahmed101Mohammed@users.noreply.github.com>
2022-11-09 12:44:58 +0200

    Complete the details of the "verifyJWT" middleware

--- middlewares/verifyJWT.js
+++ middlewares/verifyJWT.js
@@ -3,6 +3,21 @@ require('dotenv').config();
 
 const verifyJWT = (req, res, next)=>
 {
+    const authHeader = req.headers['authorization'];
+    if(!authHeader) return res.sendStatus(401);
+    console.log(authHeader);
+
+    let accessTocken = authHeader.split(' ')[1];
+    jwt.verify(
+        accessTocken,
+        process.env.ACCESS_TOCKEN_SECRET,
+        (error, decouded) =>
+        {
+            if(error) return res.sendStatus(403);
+            req.user = decouded.userName;
+            next();
+        }
+    )
 
 }
 
