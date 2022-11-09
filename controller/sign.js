f8c3e77bd40b8daa1adbbbbd2c6abe336ebeb69f
Ahmed Mohamed 3adl <80198727+Ahmed101Mohammed@users.noreply.github.com>
2022-11-09 08:13:13 +0200

    Add a new lines for sure the password is right
    - The lines added to sure that the password the user enter is the same as the password in the database.

--- controller/sign.js
+++ controller/sign.js
@@ -7,6 +7,14 @@ const signToMyAccount = (req, res)=>
 
     if(!databaseObject.isTheUserNameDublicated({userName})) return res.json({'userNameError': 'The user name is not registered before'})
 
-
+    let dataIsRigt = databaseObject.isThePasswordOfTheUserNameIsARight(password, {userName});
+    if(!dataIsRigt) 
+    {
+        return res.json({'passwordError': 'Wrong password, Try again...'})
+    }
+    else if(typeof(dataIsRigt) === Object)
+    {
+        return dataIsRigt;
+    }
     
 }
