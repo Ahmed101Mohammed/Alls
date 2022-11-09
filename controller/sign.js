eade622938189e8ab99fbf6f6c4baa3b79152dba
Ahmed Mohamed 3adl <80198727+Ahmed101Mohammed@users.noreply.github.com>
2022-11-09 08:43:10 +0200

    Modify signToMyAccount method
    Add await for methods that return a promise function.

--- controller/sign.js
+++ controller/sign.js
@@ -1,13 +1,15 @@
 let path = require('path');
 let databaseObject = require(path.join(__dirname, '..', 'objects', 'database.js'));
 
-const signToMyAccount = (req, res)=>
+const signToMyAccount = async(req, res)=>
 {
     const {userName, password} = req.body;
 
-    if(!databaseObject.isTheUserNameDublicated({userName})) return res.json({'userNameError': 'The user name is not registered before'})
+    const isTheUserNameExist = await databaseObject.isTheUserNameDublicated({userName:userName});
+    console.log(isTheUserNameExist);  // test
+    if(!isTheUserNameExist) return res.json({'userNameError': 'The user name is not registered before'})
 
-    let dataIsRigt = databaseObject.isThePasswordOfTheUserNameIsARight(password, {userName});
+    let dataIsRigt = await databaseObject.isThePasswordOfTheUserNameIsARight(password, {userName:userName});
     if(!dataIsRigt) 
     {
         return res.json({'passwordError': 'Wrong password, Try again...'})
