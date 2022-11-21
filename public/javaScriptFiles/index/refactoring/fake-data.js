3ec64ef919c09a8fea2b38578a746c81640f6aa3
Ahmed Mohamed al3adl <ahmedmohamedal3adl@gmail.com>
2022-11-21 16:04:00 +0200

    Add fack data to test the project

---
+++ public/javaScriptFiles/index/refactoring/fake-data.js
@@ -0,0 +1,14 @@
+const Categories = [
+    {
+        name: "Phones",
+        BP: 23.9,
+        products:[
+            {
+                name: "IPhone11",
+                price: 11000,
+                rate: 3.4,
+                image: ''
+            }
+        ]
+    }
+]
