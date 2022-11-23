02e31b75d601f73c390dde738ec4aaeaa474c587
Ahmed Mohamed al3adl <ahmedmohamedal3adl@gmail.com>
2022-11-23 14:37:30 +0200

    Improve the authWithJWT class

--- classes/auth.js
+++ classes/auth.js
@@ -2,13 +2,29 @@ let jwt = require('jsonwebtoken');
 
 class authWithJWT
 {
-    createTocken = (data, tockenSecterData, expiratoryDate)=>{
-        jwt.sign(
-            data,
-            tockenSecterData,
-            {expiresIn: expiratoryDate}
-        )
+    constructor()
+    {
+
+    }
+
+    createTocken = (data, tockenSecterData, expiratoryDateInString)=>{
+        try
+        {
+            let tocken = jwt.sign(
+                data,
+                tockenSecterData,
+                {expiresIn: expiratoryDateInString}
+            );
+
+            return tocken;
+        }
+        catch(_)
+        {
+            return false;
+        }
+        
+        
     }
 }
 
-module.exports = auth;
+module.exports = authWithJWT;
